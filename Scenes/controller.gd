e27d874e50b2f126a1d77ac1906171e0cae00087
ahdpeee <andreyborisov08@yandex.ru>
2026-02-17 01:55:28 +0300

    Started. Keys controller, Shader dithering

---
+++ Scenes/controller.gd
@@ -0,0 +1,56 @@
+extends Control
+
+var keyspressed = []
+@export var keysstacklen = 10
+var keysstack = []
+
+func shouldBeKeysArr(str):
+	var c = []
+	if str is String:
+		if ";" in str:
+			for x in str.split(";"): c.append(x)
+		else:
+			for x in str: c.append(x)
+	return c
+
+func inStack(str: String):
+	str = shouldBeKeysArr(str)
+	
+	var l = len(str)
+	for x in range(1, len(str) + 1):
+		if keysstack[keysstacklen-x] != str[l-x]:
+			return false
+	return true and len(str)
+func arePressed(str):
+	str = shouldBeKeysArr(str)
+	#print(str)
+	#print(keyspressed)
+	#print("")
+	for x in str:
+		if x not in keyspressed:
+			#print(x)
+			return false
+	return true and len(str)
+
+func _ready() -> void:
+	for x in range(keysstacklen):
+		keysstack.append("")
+
+func _process(delta: float) -> void:
+	pass
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		var c = (String.chr(event.unicode) if event.unicode else event.as_text()).to_lower()
+		#print(event.get_keycode_with_modifiers())
+		if event.is_pressed():
+			keysstack.remove_at(0); keysstack.append(c)
+			if c not in keyspressed:
+				keyspressed.append(c)
+			#print_debug(keyspressed)
+			#print_debug(keysstack)
+		else:
+			keyspressed.erase(c)
+		if event.is_pressed() and not event.is_echo():
+			if arePressed("asd"): print("left")
+			if arePressed("l;'"): print("right")	
