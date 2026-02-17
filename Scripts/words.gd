bb6aa7bba6c9b55fcca9decef57a36c9ffdfafdd
ahdpeee <andreyborisov08@yandex.ru>
2026-02-17 17:09:19 +0300

    i'l be back

--- Scripts/words.gd
+++ Scripts/words.gd
@@ -1,12 +1,14 @@
 extends Node3D
 
 var s = '' 
+var dic = ["GIVE", "BUY", "SELL", "NOW", "START", "MONEY", "MUST", "MORE", "WAITING"]
 var banned = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
 	'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
 	'z', 'x', 'c', 'v', 'b', 'n', 'm', 'space']
 
 func _ready() -> void:
-	AddWords(['perpetuum', 'rotation', 'rejection', 'fighter', 'magnetometr', 'sight'], 1, $ToType)
+	#AddWords(['perpetuum', 'rotation', 'rejection', 'fighter', 'magnetometr', 'sight'], 1, $ToType)
+	AddWords(dic, 1, $ToType)
 
 var ans3 = []
 func AddWords(ans, perk: int, object: MeshInstance3D):
@@ -34,19 +36,19 @@ func AddWords(ans, perk: int, object: MeshInstance3D):
 	object.mesh.text = s
 	
 func checkWords(object: MeshInstance3D):
-	if object.mesh.text.to_lower() in ans3:
+	if object.mesh.text.to_upper() in ans3:
 		return true
 
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventKey:
-		$JustTyped.mesh.text = s.to_upper()
+	if event is InputEventKey and event.is_pressed() :
 		#print_debug(event.as_text())
-		if event.is_pressed() and not event.is_echo() and not event.get_modifiers_mask() and event.as_text().to_lower() in banned:
+		if not event.get_modifiers_mask() and event.as_text().to_lower() in banned and len(s)<=9:
 			s += String.chr(event.keycode).to_lower()
-		if event.is_pressed() and not event.is_echo() and event.as_text() == 'Backspace' and len(s) > 0:
+		if event.as_text() == 'Backspace' and len(s) > 0:
 			s[-1] = ''
-		if event.is_pressed() and not event.is_echo() and event.as_text() == 'Enter' and checkWords($JustTyped):
+		$JustTyped.mesh.text = s.to_lower()
+		if checkWords($JustTyped):
 			print_debug('correct')
 			s = ''
-			AddWords(['perpetuum', 'rotation', 'rejection', 'fighter', 'magnetometr', 'sight'], 1, $ToType)
+			AddWords(dic, 1, $ToType)
