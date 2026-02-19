26dbefa5591082cc401f387f174e9898fe1b5497
ahdpeee <andreyborisov08@yandex.ru>
2026-02-19 15:57:56 +0300

    Letters now drops and moves viea letters

--- Scripts/words.gd
+++ Scripts/words.gd
@@ -1,21 +1,24 @@
 extends Node3D
 
 var s = '' 
+var sMesh = []
+var type = true
 var dic = {"LIVE":"GIVE", "GUY":"BUY", "BELL":"SELL", "WOW":"NOW", "MART":"START", "HONEY":"MONEY", \
 	"JUST":"MUST", "LORE":"MORE", "BAITING":"WAITING"}
 var banned = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
 	'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
 	'z', 'x', 'c', 'v', 'b', 'n', 'm', 'space']
+var activeLetterIndex = -1
 
 func _ready() -> void:
 	#AddWords(['perpetuum', 'rotation', 'rejection', 'fighter', 'magnetometr', 'sight'], 1, $ToType)
 	AddWords(dic.values(), 1, $ToType)
 
 var ans3 = []
-func AddWords(ans, perk: int, object: MeshInstance3D):
+func AddWords(ans : Array, perk: int, object: MeshInstance3D):
 	
 	ans3 = []
-	while len(ans3) < 3:
+	while len(ans3) < 1:
 		var n = randi() % len(ans)
 		if ans[n] not in ans3:
 			ans3.append(ans[n])
@@ -44,11 +47,24 @@ func checkWords(object: MeshInstance3D):
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey and event.is_pressed() :
 		#print_debug(event.as_text())
-		if not event.get_modifiers_mask() and event.as_text().to_lower() in banned and len(s)<=9:
+		if not event.get_modifiers_mask() and event.as_text().to_lower() in banned and len(s)<=9 and type:
+			activeLetterIndex += 1
+			sMesh.append($makeView.addLetter(\
+			String.chr(event.keycode).to_lower(), activeLetterIndex-len($ToType.mesh.text)/2, 0,\
+			$makeView/TypingArea.position+Vector3(0,1,0), $makeView/TypingArea.rotation).letter)
 			s += String.chr(event.keycode).to_lower()
-		if event.as_text() == 'Backspace' and len(s) > 0:
+		if event.as_text() == 'Backspace' and activeLetterIndex >= 0:
+			$makeView/Letters.remove_child(sMesh[activeLetterIndex])
+			$makeView/Letters.remove_child($makeView/Letters.get_child(activeLetterIndex))
+			activeLetterIndex -= 1
 			s[-1] = ''
 		$JustTyped.mesh.text = s.to_lower()
 		if checkWords($JustTyped):
-			s = ''
-			AddWords(dic.values(), 1, $ToType)
+			$makeView.move = true
+			type = false
+			#s = ''
+			#print(sMesh)
+			#for x in range(activeLetterIndex):
+				#$makeView/Letters.remove_child(sMesh[-x])
+			#activeLetterIndex = -1
+			#AddWords(dic.values(), 1, $ToType)
