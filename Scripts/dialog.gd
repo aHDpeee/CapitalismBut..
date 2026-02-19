09d7c817291ae8e6e7d5ddd8c74f9caf247d0fad
16BiTOViCH <dimasrjabikin@gmail.com>
2026-02-19 21:14:23 +0300

    Add files via upload

--- Scripts/dialog.gd
+++ Scripts/dialog.gd
@@ -1,9 +1,12 @@
 extends Node3D
 
+@onready var lInstance = preload("res://Scenes/button.tscn")
 var s =''
 
 func _ready() -> void:
 	dialog('blablabalblablablalblablbalb', '', 50.0, ['fert', 'gert', 'tert'])
+	#await get_tree().create_timer(3).timeout
+	#dialog('fucjk', 'itan', 50.0, ['reta', 'gerta', 'terta'])
 	
 func dialog(text: String, name := '', speed := 50.0, choose := []):
 	if name == '': s = '... \n'
@@ -15,7 +18,15 @@ func dialog(text: String, name := '', speed := 50.0, choose := []):
 		$DialogTextLabel.text = s
 		
 	if choose != []:
+		for child in $VBoxContainer.get_children():
+			$VBoxContainer.remove_child(child)
+			child.queue_free()
+			
 		for i in range(0, len(choose)):
-			$ItemList.add_item(choose[i])
-			$ItemList.sort_items_by_text()
-	#$ItemList.is_anything_selected().connect()
+			var butt : Button = lInstance.instantiate()
+			butt.text = choose[i]
+			$VBoxContainer.add_child(butt)
+			butt.pres.connect(choosen)
+
+func choosen(s1: String):
+	return s1
