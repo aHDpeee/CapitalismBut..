6218e3611b0533ba3d062b0f9089596550c1f4f6
ahdpeee <andreyborisov08@yandex.ru>
2026-02-21 00:48:25 +0300

    sorry next day

--- Scripts/over_camera.gd
+++ Scripts/over_camera.gd
@@ -8,7 +8,7 @@ var t : Tween
 func myTween():
 	var t = get_tree().create_tween()
 	t.set_parallel(true)
-	t.set_ease(Tween.EASE_IN_OUT)
+	t.set_ease(Tween.EASE_IN)
 	t.set_trans(Tween.TRANS_QUAD)
 	return t
 
@@ -17,13 +17,13 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
 		if event.is_pressed():
 			print('key')
-			if Controller.inStack("/z") and status== "table":
+			if Controller.arePressed("/z")   and status== "table":
 				status = "window"; 
 				if t && t.is_running(): t.stop();
 				print("window")
 				t = myTween()
-				t.tween_property(self, "position", Vector3(0,defPos.y,0.8), 1.5)
-				t.tween_property(self, "rotation_degrees", Vector3(0,0,0), 1.5)
+				t.tween_property(self, "position", Vector3(0,2.502*0.7,1.2), 1.5)
+				t.tween_property(self, "rotation", Vector3(0,0,0), 1.5)
 				t.play()
 				if Controller.inStack("]q"):
 					pass
