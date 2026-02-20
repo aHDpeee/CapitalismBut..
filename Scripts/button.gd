a011d90af0a4a56d7928aa894298397a2d30faa0
ahdpeee <andreyborisov08@yandex.ru>
2026-02-20 21:40:46 +0300

    dialogue on main scene

--- Scripts/button.gd
+++ Scripts/button.gd
@@ -3,4 +3,5 @@ extends Button
 signal pres(txt:String)
 
 func _on_pressed() -> void:
+	print(text)
 	pres.emit(text)
--- Scripts/dialog.gd
+++ Scripts/dialog.gd
@@ -1,10 +1,10 @@
-extends Node3D
+extends Control
 
 @onready var lInstance = preload("res://Scenes/button.tscn")
 var s =''
 
-func _ready() -> void:
-	dialog('blablabalblablablalblablbalb', '', 50.0, ['fert', 'gert', 'tert'])
+#func _ready() -> void:
+	#dialog('blablabalblablablalblablbalb', '', 50.0, ['fert', 'gert', 'tert'])
 	#await get_tree().create_timer(3).timeout
 	#dialog('fucjk', 'itan', 50.0, ['reta', 'gerta', 'terta'])
 	
@@ -15,18 +15,19 @@ func dialog(text: String, name := '', speed := 50.0, choose := []):
 	for i in range(0, len(text)):
 		await get_tree().create_timer(1/speed).timeout
 		s+=text[i]
-		$DialogTextLabel.text = s
+		$VBox/DialogTextLabel.text = s
 		
 	if choose != []:
-		for child in $VBoxContainer.get_children():
-			$VBoxContainer.remove_child(child)
+		for child in $VBox/Options.get_children():
+			$VBox/Options.remove_child(child)
 			child.queue_free()
 			
 		for i in range(0, len(choose)):
 			var butt : Button = lInstance.instantiate()
 			butt.text = choose[i]
-			$VBoxContainer.add_child(butt)
+			$VBox/Options.add_child(butt)
 			butt.pres.connect(choosen)
 
 func choosen(s1: String):
+	print(s1)
 	return s1
