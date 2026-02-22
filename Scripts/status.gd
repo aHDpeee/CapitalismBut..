db6d9009708328d2e9790b1486cfa1f7c61f0aa3
ahdpeee <andreyborisov08@yandex.ru>
2026-02-23 00:28:46 +0300

    if it isn't usless

--- Scripts/status.gd
+++ Scripts/status.gd
@@ -4,8 +4,13 @@ var makeIndex = 0
 var carma = 0
 
 
-var hints = ["res://Textures/GIVEhint.png", "res://Textures/PRICEhint.png", "res://Textures/BUYhint.png"]
+var hints = ["res://Textures/GIVEhint.png",\
+"res://Textures/PRICEhint.png",\
+"res://Textures/BUYhint.png",\
+"res://Textures/FREEhint.png"
+]
 var makeScene = [preload("res://Scenes/LevelTypes/GIVE.tscn"),\
 preload("res://Scenes/LevelTypes/PRICES.tscn"),\
 preload("res://Scenes/LevelTypes/BUY.tscn"),\
+preload("res://Scenes/LevelTypes/FREE.tscn")
 ]
